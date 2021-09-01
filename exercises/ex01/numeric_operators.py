fda80152dce582c58a90b16334764d4db7644c6d
Annalisa Tritt <antritt@live.unc.edu>
2021-08-31 21:39:20 -0400

    progress on exercise 1

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,18 @@
+"""Numeric Operators Program!!"""
+__author__ = "730228132"
+num: str = input("Left-hand side : ")
+num: str = input("Right-hand side : ")
+x = 17
+y = 8
+t1_new = int(x) ** int(y)
+ro = str(t1_new)
+print("17 ** 8 " + "is " + ro)
+t2_new = int(x) / int(y)
+r1 = str(t2_new)
+print("17 / 8 " + "is " + r1)
+t3_new = int(x) // int(y)
+r2 = str(t3_new)
+print("17 // 8 " + "is " + r2)
+t4_new = int(x) % int(y)
+r3 = str(t4_new)
+print("17 % 8 " + "is " + r3)
